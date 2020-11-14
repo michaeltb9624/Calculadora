63cb7b05711da9135b5420392fd0d0c0f3de7262
Michael Torres <michaeltorresberdugo@gmail.com>
2020-11-14 10:33:34 -0500

    suma de dos numeros

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText num1,num2;
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        num1 = findViewById(R.id.txtNumeroUno);
+        num2 = findViewById(R.id.txtNumeroDos);
+        resultado = findViewById(R.id.lblResultado);
+    }
+
+    public void calcular(View v){
+        double n1, n2, suma;
+
+        n1 = Double.parseDouble(num1.getText().toString());
+        n2 = Double.parseDouble(num2.getText().toString());
+        suma = n1+n2;
+        resultado.setText(""+suma);
+        //resultado.setText(String.valueOf(suma));
     }
 }
