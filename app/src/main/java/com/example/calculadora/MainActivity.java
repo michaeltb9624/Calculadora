3270c71ca8573c80c26e833ba8a08eac8a725fd2
Michael Torres <michaeltorresberdugo@gmail.com>
2020-11-14 14:07:08 -0500

    Operaciones basicas y control de division por cero

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText num1,num2;
     private TextView resultado;
+    private Spinner combo;
+    private  String opciones[];
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +25,45 @@ public class MainActivity extends AppCompatActivity {
         num1 = findViewById(R.id.txtNumeroUno);
         num2 = findViewById(R.id.txtNumeroDos);
         resultado = findViewById(R.id.lblResultado);
+        combo = findViewById(R.id.cmbOperaciones);
+
+        opciones = getResources().getStringArray(R.array.operaciones);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones);
+        combo.setAdapter(adapter);
     }
 
     public void calcular(View v){
         double n1, n2, suma;
+        int op;
         if(validar()) {
             n1 = Double.parseDouble(num1.getText().toString());
             n2 = Double.parseDouble(num2.getText().toString());
-            suma = n1 + n2;
-            resultado.setText("" + suma);
+            op = combo.getSelectedItemPosition();
+            switch (op){
+                case 0:
+                    suma = n1 + n2;
+                    break;
+                case 1:
+                    suma = n1 - n2;
+                    break;
+                case 2:
+                    suma = n1 * n2;
+                    break;
+                case 3:
+                    suma = n1 / n2;
+                    break;
+                default:
+                    throw new IllegalStateException("Operacion no permitida: " + op);
+            }
+            resultado.setText(String.format("%.2f",suma));
             //resultado.setText(String.valueOf(suma));
         }
     }
 
     public boolean validar(){
+        int op = combo.getSelectedItemPosition();
+
         if(num1.getText().toString().isEmpty()){
             num1.setError(getString(R.string.mensaje_error_numero_uno));
             num1.requestFocus();
@@ -43,6 +74,11 @@ public class MainActivity extends AppCompatActivity {
             num2.requestFocus();
             return false;
         }
+        double n2 = Double.parseDouble(num2.getText().toString());
+        if(n2 == 0 && op == 3){
+            num2.setError(getString(R.string.mensaje_error_divison_cero));
+            num2.requestFocus();
+        }
      return true;
     }
 
@@ -50,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
         num1.setText("");
         num2.setText("");
         resultado.setText("");
+        combo.setSelection(0);
         num1.requestFocus();
     }
 }
