c3ce07b8c1c9488cfef73c0030b086f5d9bd59b6
Michael Torres <michaeltorresberdugo@gmail.com>
2020-11-14 11:26:33 -0500

    sumando correctamente y validando

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -45,4 +45,11 @@ public class MainActivity extends AppCompatActivity {
         }
      return true;
     }
+
+    public void limpiar(View v){
+        num1.setText("");
+        num2.setText("");
+        resultado.setText("");
+        num1.requestFocus();
+    }
 }
